88ca145b4faab91cf7b08717e61f76ddf310f0da
Weinberger <daniel.weinberger@yale.edu>
2022-01-05 23:56:34 -0500

    set up functions to extrapolate baseline to 2020 with inla

--- R/pred.interval.func.R
+++ R/pred.interval.func.R
@@ -1,5 +1,6 @@
 pred.interval.func <- function(sample.ds,  dist=c('nb','poisson')){
-  mu1 <- exp(sample.ds$latent[grep('Predictor', row.names(sample.ds$latent))])
+  #mu1 <- exp(sample.ds$latent[grep('Predictor', row.names(sample.ds$latent))])
+  mu1 <- exp(sample.ds)
   if(dist=='nb'){
     nb.size1 = sample.ds$hyperpar
     pred <- replicate(10, rnbinom(n=length(mu1), mu=mu1, size=nb.size1), simplify = 'array')
